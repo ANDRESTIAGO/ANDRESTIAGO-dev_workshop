c0c0506f147b5e7b1fb3b9a73d3dc9df6e1cf106
ANDRESTIAGO <drestiagomorales@gmail.com>
2025-03-09 17:24:32 -0500

    Se arreglo la funcion de ecuacion recta

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -130,19 +130,6 @@ class Geometria:
         B = -(x2 - x1)
         C = -(A * x1 + B * y1)
         return A, B, C
-        """
-        Obtiene los coeficientes de la ecuación de una recta en la forma Ax + By + C = 0.
-        
-        Args:
-            x1 (float): Coordenada x del primer punto
-            y1 (float): Coordenada y del primer punto
-            x2 (float): Coordenada x del segundo punto
-            y2 (float): Coordenada y del segundo punto
-            
-        Returns:
-            tuple: Coeficientes (A, B, C) de la ecuación de la recta
-        """
-        pass
     
     def area_poligono_regular(self, num_lados, lado, apotema):
         """
